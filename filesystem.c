0bf6d6b6401c95a6bc54bff04d42fc2f6347a68a
Sam Miller <sithpow@gmail.com>
2018-11-05 21:36:05 -0600

    Various chages, start logic of  delete_file

--- filesystem.c
+++ filesystem.c
@@ -5,92 +5,114 @@
 #include "filesystem.h"
 
 //Inode
-struct INode{
+struct INode
+{
     char name[255];
-    int  directBlock[12];
-    int  indirectBlock;
+    int directBlock[12];
+    int indirectBlock;
 
-}INode;
+} INode;
 
 // main private file type
-struct FileInternals{
+struct FileInternals
+{
+    //TODO: More stuff here...
     FILE *fp;
 
-
-}FileInternals;
+} FileInternals;
 
 // file type used by user code
-typedef struct FileInternals* File;
-
-
+typedef struct FileInternals *File;
 
 FSError fserror;
 FileMode mode;
 
-File open_file(char *name, FileMode mode){
-	return NULL;
+File open_file(char *name, FileMode mode)
+{
+    return NULL;
 }
 
 // create and open new file with pathname 'name' and access mode 'mode'.  Current file
 // position is set at byte 0.  Returns NULL on error. Always sets 'fserror' global.
-File create_file(char *name, FileMode mode){
-	return NULL;
+File create_file(char *name, FileMode mode)
+{
+    return NULL;
 }
 
 // close 'file'.  Always sets 'fserror' global.
-void close_file(File file){
-	return;
+void close_file(File file)
+{
+    return;
 }
 
 // read at most 'numbytes' of data from 'file' into 'buf', starting at the
 // current file position.  Returns the number of bytes read. If end of file is reached,
 // then a return value less than 'numbytes' signals this condition. Always sets
 // 'fserror' global.
-unsigned long read_file(File file, void *buf, unsigned long numbytes){
-	return 1;
+unsigned long read_file(File file, void *buf, unsigned long numbytes)
+{
+    return 1;
 }
 
 // write 'numbytes' of data from 'buf' into 'file' at the current file position.
 // Returns the number of bytes written. On an out of space error, the return value may be less than 'numbytes'.
 // Always sets 'fserror' global.
-unsigned long write_file(File file, void *buf, unsigned long numbytes){
-	return 1;
+unsigned long write_file(File file, void *buf, unsigned long numbytes)
+{
+    return 1;
 }
 
 // sets current position in file to 'bytepos', always relative to the
 // beginning of file.  Seeks past the current end of file should
 // extend the file. Returns 1 on success and 0 on failure.
 // Always sets 'fserror' global.
-int seek_file(File file, unsigned long bytepos){
-	return 1;
+int seek_file(File file, unsigned long bytepos)
+{
+    return 1;
 }
 
 // returns the current length of the file in bytes.
 // Always sets 'fserror' global.
-unsigned long file_length(File file){
-	return 1;
+unsigned long file_length(File file)
+{
+    return 1;
 }
 
 // deletes the file named 'name', if it exists. Returns 1 on success, 0 on failure.
 // Always sets 'fserror' global.
-int delete_file(char *name){
-	return 1;
+int delete_file(char *name)
+{
+    // Delete file successfully
+    If(1])
+    {
+        return 1;
+    }
+    else
+    {
+        fserror=FS_FILE_NOT_FOUND;
+        return 0;
+    }
+
 }
 
 // determines if a file with 'name' exists and returns 1 if it exists, otherwise 0.
 // Always sets 'fserror' global.
-int file_exists(char *name){
-	return 1;
+int file_exists(char *name)
+{
+
+    return 1;
 }
 
 // describe current filesystem error code by printing a descriptive message to standard error.
-void fs_print_error(void){
-    switch (fserror){
+void fs_print_error(void)
+{
+    switch (fserror)
+    {
     case FS_NONE:
         printf("FS: No error. \n");
         break;
     case FS_OUT_OF_SPACE:
-        printf("FS: The operation casued the softwaredisk to fill up. \n");
+        printf("FS: The operation caused the softwaredisk to fill up. \n");
         break;
     case FS_FILE_NOT_OPEN:
         printf("FS: Attempted read/write/close/etc. on file that isn't open. \n");
@@ -120,7 +142,6 @@ void fs_print_error(void){
         printf("FS: Unknown error code %d.\n", fserror);
         break;
     }
-
 }
 
 // filesystem error code set (set by each filesystem function)
