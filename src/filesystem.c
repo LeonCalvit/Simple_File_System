bfba6d01c5eb5cd75c4889d0797af84c5c141e9a
Sam Miller <sithpow@gmail.com>
2018-11-21 23:13:12 -0600

    formating

--- src/filesystem.c
+++ src/filesystem.c
@@ -320,14 +320,14 @@ int delete_file(char *name)
 	unsigned char* empty_buffer = calloc(SOFTWARE_DISK_BLOCK_SIZE, sizeof(unsigned char));
 	if (node->num_blocks < NUM_BLOCKS_IN_INODE) { //Block numbers aren't being stored in the indirect nodes, so deleting the INode is simpler;
 		//Goes through blocks of the INode and writes zeroes to them
-		
+
 		for (unsigned long j = 0; j < node->num_blocks; j++) {
 			write_sd_block(empty_buffer, nodes[i]->directBlock[j]);
 			flip_block_availability(nodes[i]->directBlock[j]);
 		}
 	}
 	else { //More complex case
-		
+
 		for (unsigned long j = 0; j < NUM_BLOCKS_IN_INODE; j++) { //Free all the blocks in the direct node
 			write_sd_block(empty_buffer, nodes[i]->directBlock[j]);
 			flip_block_availability(nodes[i]->directBlock[j]);
