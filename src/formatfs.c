da7d555187cc3cd97913337699234d39500a6c01
Sam Miller <sithpow@gmail.com>
2018-11-26 04:50:43 -0600

    cleanup formatfs.c

--- src/formatfs.c
+++ src/formatfs.c
@@ -4,25 +4,10 @@
 
 int main(int argc, char const *argv[])
 {
-
-    unsigned char *bitmaps[SOFTWARE_DISK_BLOCK_SIZE-2/sizeof(char)];
-
     printf("Initializing Disk...\n");
+
     // wipe disk completely
-
     init_software_disk();
-//     // My init is not as elegant as the one shown in class, at least for now...
-//     printf("Initializing Free Space Tracking... \n");
-//     //TODO: Create (2) Free space tracking blocks and mark them in use
-//     write_sd_block(bitmaps,0);
-//     write_sd_block(bitmaps,1);
-
-//     //TODO: Create inode registration blocks and mark them in use
-//    // Something like this -> write_sd_block(INode[SOFTWARE_DISK_BLOCK_SIZE/sizeof(INode)],2);
-
-
     printf("done.\n");
-
-
     return 0;
 }
